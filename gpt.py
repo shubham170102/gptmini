ed75c2edba7334c1eab88b37c0679d65ceadb7e3
shubham170102 <79875009+shubham170102@users.noreply.github.com>
2024-02-26 23:40:21 -0500

    adjusted hyperparameters

--- gpt.py
+++ gpt.py
@@ -3,17 +3,31 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # Hyperparameters
-batch_size = 64 # how many independent sequences will we process in parallel?
-block_size = 256 # what is the maximum context length for predictions?
+# batch_size = 64 # how many independent sequences will we process in parallel?
+# block_size = 256 # what is the maximum context length for predictions?
+# max_iters = 5000
+# eval_interval = 500
+# learning_rate = 3e-4
+# device = 'cuda' if torch.cuda.is_available() else 'cpu'
+# eval_iters = 200
+# n_embd = 384
+# n_head = 6
+# n_layer = 6
+# dropout = 0.2
+
+# Adjusted Hyperparameters for M1 Pro Mackbook Pro
+batch_size = 32
+block_size = 128
 max_iters = 5000
 eval_interval = 500
 learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 384
-n_head = 6
-n_layer = 6
+n_embd = 256
+n_head = 4
+n_layer = 4
 dropout = 0.2
+
 # ----------------
 
 torch.manual_seed(1337)
